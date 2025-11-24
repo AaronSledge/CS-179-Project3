ff6d51cb840e7c61c3394ee0c1793fa220a77696
AaronSledge <ahern2118@gmail.com>
2025-11-24 13:36:15 -0800

    Add in other action functions

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ from FileRead import FileRead
 
 from Matrix import matrix
 from Container import Ship
-from Operation import right
+from Operation import right, left, up, down
 
 filename = "smallshiptest.txt"
 listContainers = FileRead(filename)
@@ -18,7 +18,7 @@ for i in range(1, -1, -1):
 container = m[0][0]
 print(container.location.x)
 print(container.location.y)
-m = right(m, 0, 0)
+m = right(m, container.location.x - 1, container.location.y - 1)
 
 print("/n")
 print("/n")
@@ -29,6 +29,44 @@ for i in range(1, -1, -1):
         print(m[i][j].description, end = " ")
 
 
+container = m[0][1]
+m = left(m, container.location.x - 1, container.location.y - 1)
+
+print("/n")
+print("/n")
+for i in range(1, -1, -1):
+    for j in range(4):
+        if (i == 0 and j == 0):
+            print("/n")
+        print(m[i][j].description, end = " ")
+
+filename = "smallshiptest2.txt"
+listContainers = FileRead(filename)
+ship = Ship(listContainers, False)
+m2 = matrix(ship.listContainers)
+
+container = m2[0][0]
+m2 = up(m2, container.location.x - 1, container.location.y - 1)
+
+print("/n")
+print("/n")
+for i in range(1, -1, -1):
+    for j in range(4):
+        if (i == 0 and j == 0):
+            print("/n")
+        print(m2[i][j].description, end = " ")
+
+container = m2[1][0]
+m2 = down(m2, container.location.x - 1, container.location.y - 1)
+
+
+print("/n")
+print("/n")
+for i in range(1, -1, -1):
+    for j in range(4):
+        if (i == 0 and j == 0):
+            print("/n")
+        print(m2[i][j].description, end = " ")
 
 
 
--- Operation.py
+++ Operation.py
@@ -5,7 +5,31 @@ def right(matrix, x, y):
     if (matrix[x][y + 1].description == "UNUSED\n"):
         matrix[x][y + 1].description = matrix[x][y].description
         matrix[x][y + 1].weight = matrix[x][y].weight
-        matrix[x][y].description = "UNUSED"
+        matrix[x][y].description = "UNUSED\n"
+        matrix[x][y].weight = "{00000}"
+    return matrix
+
+def left(matrix, x, y):
+    if (matrix[x][y - 1].description == "UNUSED\n"):
+        matrix[x][y - 1].description = matrix[x][y].description
+        matrix[x][y - 1].weight = matrix[x][y].weight
+        matrix[x][y].description = "UNUSED\n"
+        matrix[x][y].weight = "{00000}"
+    return matrix
+
+def up(matrix, x, y):
+    if (matrix[x + 1][y].description == "UNUSED\n"):
+        matrix[x + 1][y].description = matrix[x][y].description
+        matrix[x + 1][y].weight = matrix[x][y].weight
+        matrix[x][y].description = "UNUSED\n"
+        matrix[x][y].weight = "{00000}"
+    return matrix
+
+def down(matrix, x, y):
+    if (matrix[x - 1][y].description == "UNUSED\n"):
+        matrix[x - 1][y].description = matrix[x][y].description
+        matrix[x - 1][y].weight = matrix[x][y].weight
+        matrix[x][y].description = "UNUSED\n"
         matrix[x][y].weight = "{00000}"
     return matrix
 
