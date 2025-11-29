6c293f998d660902858ebd70af394b3d98a68afa
Mcamacho457 <camachomichael343@gmail.com>
2025-11-28 17:25:37 -0800

    Changes to state, operation, fileread, main

--- Main.py
+++ Main.py
@@ -4,42 +4,61 @@ from Matrix import matrix
 from Container import Ship
 from Operation import right, left, up, down
 
+# smallshiptest.txt stuff
+print("--Working with smallshiptest.txt file--")
+print()
 filename = "smallshiptest.txt"
 listContainers = FileRead(filename)
 ship = Ship(listContainers, False)
 m = matrix(ship.listContainers)
 
 for i in range(1, -1, -1):
+    print(f"Row: {i+1}")
     for j in range(4):
-        if (i == 0 and j == 0):
-            print("/n")
-        print(m[i][j].location.x, m[i][j].location.y, end = " ")
+        # if (i == 0 and j == 0):
+        #     print("\n")
+        print(f"{m[i][j].description}[{str(m[i][j].location.x).zfill(2)}, {str(m[i][j].location.y).zfill(2)}]", end = " ")
+        if(j == 3):
+            print()
 
+print()
+print(f"--1st Container's Info--")
 container = m[0][0]
-print(container.location.x)
-print(container.location.y)
+print(f"{container.description}[{str(container.location.x).zfill(2)}, {str(container.location.x).zfill(2)}]")
 m = right(m, container.location.x - 1, container.location.y - 1)
 
-print("/n")
-print("/n")
+print()
+print()
 for i in range(1, -1, -1):
+    print(f"Row: {i+1}")
     for j in range(4):
-        if (i == 0 and j == 0):
-            print("/n")
-        print(m[i][j].description, end = " ")
+        # if (i == 0 and j == 0):
+        #     print("\n")
+        print(f"{m[i][j].description}[{str(m[i][j].location.x).zfill(2)}, {str(m[i][j].location.y).zfill(2)}]", end = " ")
+        if(j == 3):
+            print()
 
 
 container = m[0][1]
 m = left(m, container.location.x - 1, container.location.y - 1)
 
-print("/n")
-print("/n")
+print()
+print()
 for i in range(1, -1, -1):
+    print(f"Row: {i+1}")
     for j in range(4):
-        if (i == 0 and j == 0):
-            print("/n")
-        print(m[i][j].description, end = " ")
+        # if (i == 0 and j == 0):
+        #     print("\n")
+        print(f"{m[i][j].description}[{str(m[i][j].location.x).zfill(2)}, {str(m[i][j].location.y).zfill(2)}]", end = " ")
+        if (j == 3):
+            print()
 
+# smallshiptest2.txt stuff
+print()
+print()
+print()
+print()
+print("--Working with smallshiptest2.txt file--")
 filename = "smallshiptest2.txt"
 listContainers = FileRead(filename)
 ship = Ship(listContainers, False)
@@ -48,25 +67,26 @@ m2 = matrix(ship.listContainers)
 container = m2[0][0]
 m2 = up(m2, container.location.x - 1, container.location.y - 1)
 
-print("/n")
-print("/n")
+print()
 for i in range(1, -1, -1):
+    print(f"Row: {i+1}")
     for j in range(4):
-        if (i == 0 and j == 0):
-            print("/n")
-        print(m2[i][j].description, end = " ")
+        print(f"{m2[i][j].description}[{str(m2[i][j].location.x).zfill(2)}, {str(m2[i][j].location.y).zfill(2)}]", end = " ")
+        if (j == 3):
+            print()
 
 container = m2[1][0]
 m2 = down(m2, container.location.x - 1, container.location.y - 1)
 
 
-print("/n")
-print("/n")
+print()
+print()
 for i in range(1, -1, -1):
+    print(f"Row: {i+1}")
     for j in range(4):
-        if (i == 0 and j == 0):
-            print("/n")
-        print(m2[i][j].description, end = " ")
+        print(f"{m2[i][j].description}[{str(m2[i][j].location.x).zfill(2)}, {str(m2[i][j].location.y).zfill(2)}]", end = " ")
+        if (j == 3):
+            print()
 
 
 
--- State.py
+++ State.py
@@ -12,13 +12,19 @@ def balance_calc(left_weight, right_weight):
     return ship_status
 
 # We should check this after each iteration step by step to see if we met the goal state
+# this condition will be run multiple times until we can't decrease the weight anymore
+# this occurs when the function returns false (which is the goal state)
 def con1_balance_check(curr_state, prev_state):
-    if (curr_state.balance_con < prev_state.balance_con):
+    if (curr_state.dif_lr < prev_state.dif_lr):
         return True
+    return False
 
 # Check overall condition at each iteration to see if we potentially meet the goal state faster than con 1
+# if the first condition doesn't work, then try this. in theory, this condition should be easier to satisfy than
+# the above one
 def con2_balance_check(curr_state):
     con_2 = (curr_state.weight_left + curr_state.weight_right) * 0.10
-    if (curr_state.balance_con < con_2):
+    if (curr_state.dif_lr <= con_2):
         return True
+    return False
 
