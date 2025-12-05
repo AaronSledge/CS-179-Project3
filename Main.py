445f29e76ab9c787e76c6350a13d35d2710e11a9
kea012 <kennethea12@gmail.com>
2025-12-04 21:43:13 -0800

    Added error checking for fileread and main

--- Main.py
+++ Main.py
@@ -23,44 +23,54 @@ def RunProgram():
     count = 1
     while True:
         manifestname = input("Enter a Manifest: ")
-        if (count == 1):
-            filename = CreateLogFile()
-        count += 1
-        listContainers = FileRead(manifestname)
-        ship = Ship(listContainers, False)
-        row = 8
-        col = 12
-        maxActions = 10
-        m = matrix(ship.listContainers, row, col)
-        totalcontainers = findTotalContainers(m, row, col)
-        print(f"{manifestname[0:len(manifestname)-4]} has {totalcontainers} containers\n")
-        WriteManifestNameToFile(filename, manifestname, totalcontainers)
-        print("Computing a solution... \n")
-
-        # runs Astar to find a solution
-        movelist, new_matrix, path, totaltime, totalmoves, totalcontainers = Astar(m, row, col, maxActions)
-
-        if (totalmoves > 1):
-            print(f"Solution has been found, it will take \n {totalmoves} moves \n {totaltime} minutes \n")
+        if (manifestname == ""):
+            print("\nNothing was input as the Manifest name.\n")
+        elif (len(manifestname) < 5):
+            print("\nManifest name is not long enough.\n")
+        elif (manifestname[-4:] != ".txt"):
+            print("\nManifest name must end with .txt.\n")
         else:
-            print(f"Solution has been found, it will take \n {totalmoves} move \n {totaltime} minutes \n")
-        moveswithoutcrane = totalmoves - 2
-        timewithoutcrane = totaltime - len(movelist[0]) - len(movelist[-1])
-        #WriteTotalMoveTimeToFile(filename, moveswithoutcrane, timewithoutcrane)
-        WriteTotalMoveTimeToFile(filename, totalmoves, totaltime)
-        WritePathToFile(filename, path, totalmoves)
+            if (count == 1):
+                filename = CreateLogFile()
+            count += 1
+            listContainers = FileRead(manifestname)
+            if listContainers is not None:
+                ship = Ship(listContainers, False)
+                row = 8
+                col = 12
+                maxActions = 10
+                m = matrix(ship.listContainers, row, col)
+                totalcontainers = findTotalContainers(m, row, col)
+                if (totalcontainers < 16):
+                    print(f"{manifestname[0:len(manifestname)-4]} has {totalcontainers} containers\n")
+                    WriteManifestNameToFile(filename, manifestname, totalcontainers)
+                    print("Computing a solution... \n")
 
-        manifestname = os.path.splitext(os.path.basename(manifestname))[0]
-        manifestname += "OUTBOUND.txt"
-        WriteCycleFinished(filename, manifestname)
+                    # runs Astar to find a solution
+                    movelist, new_matrix, path, totaltime, totalmoves, totalcontainers = Astar(m, row, col, maxActions)
 
-        # creates the final manifest file that will be emailed to the captain
-        CreateFinalManifest(manifestname, new_matrix)
+                    if (totalmoves > 1):
+                        print(f"Solution has been found, it will take \n {totalmoves} moves \n {totaltime} minutes \n")
+                    else:
+                        print(f"Solution has been found, it will take \n {totalmoves} move \n {totaltime} minutes \n")
 
-        userIn = input("Enter S to stop \n")
-        if (userIn == "S" or userIn == "s"):
-            print("End program")
-            exit()
+                    WriteTotalMoveTimeToFile(filename, totalmoves, totaltime)
+                    WritePathToFile(filename, path, totalmoves)
+
+                    manifestname = os.path.splitext(os.path.basename(manifestname))[0]
+                    manifestname += "OUTBOUND.txt"
+                    WriteCycleFinished(filename, manifestname)
+
+                    # creates the final manifest file that will be emailed to the captain
+                    CreateFinalManifest(manifestname, new_matrix)
+
+                    userIn = input("Enter S to stop \n")
+                    if (userIn == "S" or userIn == "s"):
+                        print("End program")
+                        exit()
+                else:
+                    print("\nThe Manifest has more than 16 containers.\n")
+                    continue
 
 RunProgram()
 
